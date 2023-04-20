1b6e7f54ec0892d8fc1168b2a9463a88699a8b96
elsie <elsiechen915@gmail.com>
2023-04-20 19:38:48 -0400

    Build add to library button event

--- library.js
+++ library.js
@@ -2,13 +2,20 @@ let myLibrary = [];
 
 const table = document.querySelector('.table')
 const tbody = document.querySelector('.tbody');
+const addBtn = document.querySelector('#addBtn');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const read = document.querySelector('#read');
 
+// Create book instances manually
 const harryPotter = new Book('Harry Potter And The Half Blood Prince', 'J.K. Rowling', 652, false);
 const peppaPig = new Book('Peppa Pig and the Day at Snow Mountain','Nevil Astley and Mark Baker',36, true);
 const olivia = new Book('Dinner with Olivia', 'Emily Sollinger', 18, false);
 const panda = new Book('Please, Mr. Panda', 'Steve Antony', 22, true);
 const splat = new Book('Splat the Cat Twice the Mice', 'Rob Scotton', 28, true);
 
+// Add book to library manually
 addBookToLibrary(harryPotter);
 addBookToLibrary(peppaPig);
 addBookToLibrary(olivia);
@@ -17,6 +24,8 @@ addBookToLibrary(splat);
 
 display();
 
+// Add book to library event
+addBtn.addEventListener('click', addToLibrary);
 
 function Book(title, author, pages, read) {
   // the constructor...
@@ -26,9 +35,20 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+// Add book to library manually
 function addBookToLibrary(book) {
-  // do stuff here
   myLibrary.push(book);
+}
+
+// Add book to library by form
+function addToLibrary(){
+    let book = new Book(
+        title.value, 
+        author.value,
+        pages.value,
+        read.value );
+    myLibrary.push(book);
+    display();
 }
 
 function display(){
@@ -53,3 +73,5 @@ function display(){
         tbody.appendChild(tr);
     }
 }
+
+
