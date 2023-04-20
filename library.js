aa7ed61b007e4fd10bacb53a25d356c749e1fadf
elsie <elsiechen915@gmail.com>
2023-04-19 22:47:13 -0400

    Create myLibray array, table, Book constructor, addBookToLibrary function, display function

---
+++ library.js
@@ -0,0 +1,56 @@
+let myLibrary = [];
+
+const table = document.querySelector('.table')
+const tbody = document.querySelector('.tbody');
+
+const harryPotter = new Book('Harry Potter And The Half Blood Prince', 'J.K. Rowling', 652, false);
+const peppaPig = new Book('Peppa Pig and the Day at Snow Mountain','Nevil Astley and Mark Baker',36, true);
+const olivia = new Book('Dinner with Olivia', 'Emily Sollinger', 18, false);
+const panda = new Book('Please, Mr. Panda', 'Steve Antony', 22, true);
+const splat = new Book('Splat the Cat Twice the Mice', 'Rob Scotton', 28, true);
+
+addBookToLibrary(harryPotter);
+addBookToLibrary(peppaPig);
+addBookToLibrary(olivia);
+addBookToLibrary(panda);
+addBookToLibrary(splat);
+
+display();
+
+
+function Book(title, author, pages, read) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(book) {
+  // do stuff here
+  myLibrary.push(book);
+}
+
+function display(){
+    for(let book of myLibrary){
+        let tr = document.createElement('tr');
+        let td1 = document.createElement('td');
+        td1.textContent = book.title;
+        tr.appendChild(td1);
+
+        let td2 = document.createElement('td');
+        td2.textContent = book.author;
+        tr.appendChild(td2);
+
+        let td3 = document.createElement('td');
+        td3.textContent = book.pages;
+        tr.appendChild(td3);
+
+        let td4 = document.createElement('td');
+        td4.textContent = book.read;
+        tr.appendChild(td4);
+
+        tbody.appendChild(tr);
+        console.log(table);
+    }
+}
